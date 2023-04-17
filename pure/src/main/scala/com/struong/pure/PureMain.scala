7232f301b45bbb0fa90ebb58a95193bc773205b3
Steven Truong <>
2023-04-17 15:14:56 +0100

    move http server creation

--- pure/src/main/scala/com/struong/pure/PureMain.scala
+++ pure/src/main/scala/com/struong/pure/PureMain.scala
@@ -3,21 +3,42 @@ package com.struong.pure
 import cats.effect._
 import cats.implicits._
 import com.comcast.ip4s.{Host, Port}
-import com.typesafe.config._
-import config.ServiceConf
+import config.{ApiConf, ServiceConf}
 import db.{DatabaseMigrator, DoobieRepository, FlywayDatabaseMigrator}
 import doobie._
 import eu.timepit.refined.auto._
 import eu.timepit.refined.pureconfig._
 import http.Routes.{ProductRoutes, ProductsRoutes}
+import org.http4s.HttpApp
 import org.http4s.ember.server.EmberServerBuilder
 import org.http4s.implicits._
 import pureconfig._
 import pureconfig.generic.auto._
 
-object PureMain extends IOApp {
-  override def run(args: List[String]): IO[ExitCode] = {
+object PureMain extends IOApp.Simple {
+  object MkHttpServer {
+    def run[F[_]: Async](cfg: ApiConf, httpApp: HttpApp[F]): F[Nothing] = {
+      for {
+        _ <-
+          EmberServerBuilder
+            .default[F]
+            .withPort(
+              Port
+                .fromInt(cfg.port.value)
+                .getOrElse(throw new RuntimeException("No port found"))
+            )
+            .withHost(
+              Host
+                .fromString(cfg.host.value)
+                .getOrElse(throw new RuntimeException("No host found"))
+            )
+            .withHttpApp(httpApp)
+            .build
+      } yield ()
+    }.useForever
+  }
 
+  override def run: IO[Unit] = {
     val migrator: DatabaseMigrator[IO] = new FlywayDatabaseMigrator
 
     val serviceConf = ConfigSource.default.loadOrThrow[ServiceConf]
@@ -25,11 +46,12 @@ object PureMain extends IOApp {
     val apiConf = serviceConf.api
 
     for {
-      migrate <- migrator.migrate(
-        databaseConf.url,
-        databaseConf.user,
-        databaseConf.pass
-      )
+      migrate <-
+        migrator.migrate(
+          databaseConf.url,
+          databaseConf.user,
+          databaseConf.pass
+        )
       tx = Transactor.fromDriverManager[IO](
         databaseConf.driver,
         databaseConf.url,
@@ -40,23 +62,7 @@ object PureMain extends IOApp {
       productsRoutes = new ProductsRoutes(repo)
       productRoutes = new ProductRoutes(repo)
       routes = productRoutes.routes <+> productsRoutes.routes
-      server <- EmberServerBuilder
-        .default[IO]
-        .withPort(
-          Port
-            .fromInt(apiConf.port.value)
-            .getOrElse(throw new RuntimeException("No port found"))
-        )
-        .withHost(
-          Host
-            .fromString(apiConf.host.value)
-            .getOrElse(throw new RuntimeException("No host found"))
-        )
-        .withHttpApp(routes.orNotFound)
-        .build
-        .use(_ => IO.never)
-        .as(ExitCode.Success)
+      server <- MkHttpServer.run[IO](apiConf, routes.orNotFound)
     } yield server
-
   }
 }
